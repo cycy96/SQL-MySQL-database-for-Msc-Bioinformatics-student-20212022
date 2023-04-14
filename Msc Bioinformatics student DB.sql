32727e01f856a15b872a456d9d44c2f161c5c374
LEONG CHEE YAN <127811480+cycy96@users.noreply.github.com>
2023-04-14 15:10:40 +0800

    Update Msc Bioinformatics student DB.sql

--- Msc Bioinformatics student DB.sql
+++ Msc Bioinformatics student DB.sql
@@ -15,8 +15,6 @@ city varchar(20)
 show tables;
 select * from student;
 
-
-
 INSERT INTO student VALUES(1, 'S2111011', 'Muhammad Fauzan Zainudin', 32, 'M','Malay', 'Malaysia', 'Kuala Lumpur');
 INSERT INTO student (p_no, matric_id, s_name, age, gender, race, country) VALUES(2, 'S2119803', 'Ziwei Fang', 30, 'F','Chinese', 'China');
 INSERT INTO student (p_no, matric_id, s_name, age, gender, race, country) VALUES(3, 'S2123100', 'Hui Lin', 29, 'F','Chinese', 'China');
@@ -24,5 +22,3 @@ INSERT INTO student VALUES(4, 'S2124600', 'Yu Xian Yap', 24, 'F','Chinese', 'Mal
 INSERT INTO student VALUES(5, 'S2108076', 'Leong Chee Yan', 27, 'F','Chinese', 'Malaysia', 'Ipoh');
 INSERT INTO student VALUES(6, 'S2100766', 'Tan Min Hui', 38, 'F','Chinese', 'Malaysia', 'Kuala Lumpur');
 INSERT INTO student VALUES(7, 'S2121628', 'Nurhafizah Zalani', 27, 'F','Malay', 'Malaysia', 'Kuala Lumpur');
-
-DROP tables student;
